4d3338253504c9534bc5d827b22b672ea0d61726
csm <csm009177@gmail.com>
2023-10-23 21:44:21 +0900

    make server

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log("hello")
+const http = require('http');
+const fs = require('fs');
+
+let serv = http.createServer((req,res)=> {
+  if(req.method === 'GET'){
+    console.log(req.method);
+    res.end(req.method);
+  }
+})
+
