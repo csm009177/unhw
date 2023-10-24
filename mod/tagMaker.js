a5d87517523834d6d4babbb0fe37db31174529d6
csm <csm009177@gmail.com>
2023-10-25 08:33:29 +0900

    want make tagMaker2 about make blank

--- mod/tagMaker.js
+++ mod/tagMaker.js
@@ -1,7 +1,15 @@
 function tagMaker(tagName, content, type, style){
-if(typeof(tagName)==='string'&& typeof(style)==='string'){
-  return `<${tagName} type="${type}" style ="${style}">${content}</${tagName}>`
-  } else { console.log(`${tagName} and ${style} must be string`)}
+  if(typeof(tagName)==='string'&& typeof(style)==='string'){
+    return `<${tagName} type="${type}" style ="${style}">${content}</${tagName}>`
+    } else { console.log(`${tagName} and ${style} must be string`)}
 }
 
-module.exports = tagMaker;
+
+
+function tagMaker2(tagName, content, type, style){
+  if(typeof(tagName)==='string'&& typeof(style)==='string'){
+    return `<${tagName} ${type} style ="${style}">${content}</${tagName}>`
+    } else { console.log(`${tagName} and ${style} must be string`)}
+}
+
+console.log(tagMaker2('tagName', 'content', 'type', 'style'))
