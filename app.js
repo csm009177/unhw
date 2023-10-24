3315516fe8c4f6be3454382efc7305d9fc8255cb
csm <csm009177@gmail.com>
2023-10-24 09:09:56 +0900

    put comment about module

--- app.js
+++ app.js
@@ -1,22 +1,29 @@
+// http, fs
 const http = require('http');
 const fs = require('fs');
+// custom module
 const contenType = require('./mod/contenType');
 const checkreq = require('./mod/checkreq');
+const docMaker = require('./doc/doc');
+// custom module use
 let contHtml = contenType('text/html','utf8')
+let testDoc = docMaker('main', 'contents test')
 
 
-
+// make server
 let serv = http.createServer((req,res)=> {
   if(req.method === 'GET' && req.url === '/'){
     // console.log(req.method);
     // console.log(req.url);
     checkreq(req.method, req.url);
     res.writeHead(200, contHtml); 
-    res.end('hello');
+    res.end(testDoc);
   }
 })
 
+// port
 let port =3217;
+// server listen
 serv.listen(port, () => {
   console.log(`
 아래의 링크를 Clt와 함께 누르세요
