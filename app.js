4a9515202acdb40ea819bd3e3960149f055e5ca4
csm <csm009177@gmail.com>
2023-10-25 16:56:30 +0900

    fix font-size

--- app.js
+++ app.js
@@ -15,14 +15,14 @@ const toggleMaker = require('./mod/toggleMaker');
 let contHtml = contenType('text/html','utf8');
 // submit Bnt
 let submitBnt   = tagMaker('button',  'search',  '', 
-  'width:15vw; height:10vh; background:darkgray;');
+  'width:15vw; height:10vh; background:darkgray; font-size: 7vh;');
 // input
 let searchInput = tagMaker('input',   '',     'type="text"', 
 'width:40vw; height:10vh; background:white;');
 // toggle tag
 let toggleScript = toggleMaker('tog', 'User', 'green', 'Expert', 'red');
 let toggleTag   = tagMaker('button',  '',     'id= "tog" type="button" value="" ', 
-  'width:15vw; height:10vh; background:darkgray;');
+  'width:15vw; height:10vh; background:darkgray; font; font-size: 7vh;');
 //cont
 let cont        = tagMaker('div',     toggleTag+searchInput+submitBnt, '', 
   'display:flex; width:70vw; height:10vh; background:black;');
