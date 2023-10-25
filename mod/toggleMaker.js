5f3f86fb7d716316de902294c78927fd1328824c
csm <csm009177@gmail.com>
2023-10-25 09:44:38 +0900

    put tagetElement init to togColor

--- mod/toggleMaker.js
+++ mod/toggleMaker.js
@@ -1,19 +1,20 @@
-function toggleMaker(tagetId, before, aftrer) {
+function togColor(tagetId, before, after) {
   let tagetId = (tagetId+'tog');
-  let tagetId = document.getElementById(tagetId)
+  let targetElement = document.getElementById(tagetId)
   let togHandle = true;
-  toggle.addEventListener('click', ()=> {
+  targetElement.addEventListener('click', ()=> {
     if (togHandle === true) {
       togHandle = false;
-      tagetId.style.color = "red";
+      targetElement.style.color = `${before}`;
       console.log(togHandle);
       // 핸들러가 참이라면 핸들러를 거짓으로 바꿔줘
     } else {
       togHandle = true; // 아니라면 핸들러를 참으로 바꿔줘
-      tagetId.style.color = "green";
+      targetElement.style.color = `${after}`;
       console.log(togHandle);
     }
   })
+    // 초기 상태 설정
+    targetElement.style.color = togHandle ? before : after;
 }
 
-module.exports = toggleMaker;
