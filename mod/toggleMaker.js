aff9702b95bc598033c7135233557ba8dfcf0123
csm <csm009177@gmail.com>
2023-10-25 11:44:50 +0900

    try to make toggle with toggle chng color and toggle chng  text

--- mod/toggleMaker.js
+++ mod/toggleMaker.js
@@ -22,27 +22,27 @@ function togColor(doc ,tagetId, before, after) {
 
 
 
-function togColor(tagetId, before, after){
-  let toggle = document.getElementById('tog');
-  toggle.innerHTML = "User";
-  toggle.style.color = "green";
+function toggleMaker(tagetId, beforeText, beforeColor, afterColor, afterText){
+  let toggle = document.getElementById(tagetId);
+  toggle.innerHTML = beforeText;
+  toggle.style.color = beforeColor;
   // targetElement.style.color = togHandle ? before : after;
   // let togHandle = true;
     toggle.addEventListener('click', (eventTarget)=> {
-      if (toggle.innerHTML==="User") {
-        toggle.style.color = "red";
-        toggle.innerHTML = "Expert"
+      if (toggle.innerHTML===beforeText) {
+        toggle.innerHTML = afterText;
+        toggle.style.color = afterColor;
         console.log(togHandle);
         // 핸들러가 참이라면 핸들러를 거짓으로 바꿔줘
       } else {
         togHandle = true; // 아니라면 핸들러를 참으로 바꿔줘
-        toggle.style.color = "green";
-        toggle.innerHTML = "User";
+        toggle.innerHTML = beforeText;
+        toggle.style.color = beforeColor;
         console.log(togHandle);
       }
     })  
 }
 
-
+module.exports=toggleMaker;
 
 
