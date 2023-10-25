33bcb36a9be03e03fa881fe63f7557fddc1321b4
csm <csm009177@gmail.com>
2023-10-25 11:38:42 +0900

    try renewal for divide toggleMaker module

--- mod/toggleMaker.js
+++ mod/toggleMaker.js
@@ -20,6 +20,29 @@ function togColor(doc ,tagetId, before, after) {
   </Script>`
 }
 
-module.exports= togColor;
 
-console.log(togColor('tog', 'green', 'red'));
+
+function togColor(tagetId, before, after){
+  let toggle = document.getElementById('tog');
+  toggle.innerHTML = "User";
+  toggle.style.color = "green";
+  // targetElement.style.color = togHandle ? before : after;
+  // let togHandle = true;
+    toggle.addEventListener('click', (eventTarget)=> {
+      if (toggle.innerHTML==="User") {
+        toggle.style.color = "red";
+        toggle.innerHTML = "Expert"
+        console.log(togHandle);
+        // 핸들러가 참이라면 핸들러를 거짓으로 바꿔줘
+      } else {
+        togHandle = true; // 아니라면 핸들러를 참으로 바꿔줘
+        toggle.style.color = "green";
+        toggle.innerHTML = "User";
+        console.log(togHandle);
+      }
+    })  
+}
+
+
+
+
