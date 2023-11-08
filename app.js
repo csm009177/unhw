2332121ff1af313a5689ed4fa6dba9ff3677e872
csm <csm009177@gmail.com>
2023-11-08 10:43:38 +0900

    want divide route

--- app.js
+++ app.js
@@ -9,30 +9,32 @@ const tagMaker = require('./mod/tagMaker');
 const { styleWhite, styleDark, styleGray } = require('./mod/asteriskStyle');
 const arrange = require('./mod/contentArrange');
 const toggleMaker = require('./mod/toggleMaker');
+const rootRoute = require('./route/route');
 
 
 // custom module use
-let contHtml = contenType('text/html','utf8');
-// submit Bnt
-let submitBnt   = tagMaker('button',  'search',  '', 
-  'width:15vw; height:10vh; background:darkgray; font-size: 7vh;');
-// input
-let searchInput = tagMaker('input',   '',     'type="text"', 
-'width:40vw; height:10vh; background:white;');
-// toggle tag
-let toggleScript = toggleMaker('tog', 'User', 'green', 'Expert', 'red');
-let toggleTag   = tagMaker('button',  '',     'id= "tog" type="button" value="" ', 
-  'width:15vw; height:10vh; background:darkgray; font; font-size: 7vh;');
-//cont
-let cont        = tagMaker('div',     toggleTag+searchInput+submitBnt, '', 
-  'display:flex; width:70vw; height:10vh; background:black;');
-// bigcont is body cont
-let bigCont     = tagMaker('div',     cont,    '', 
-  arrange('center', 'center')+'width:100vw; height:100vh; background:black;');
-  // doc
-let mainDoc = docMaker('main', styleGray(), bigCont+toggleScript);
+// let contHtml = contenType('text/html','utf8');
+// // submit Bnt
+// let submitBnt   = tagMaker('button',  'search',  '', 
+//   'width:15vw; height:10vh; background:darkgray; font-size: 7vh;');
+// // input
+// let searchInput = tagMaker('input',   '',     'type="text"', 
+// 'width:40vw; height:10vh; background:white;');
+// // toggle tag
+// let toggleScript = toggleMaker('tog', 'User', 'green', 'Expert', 'red');
+// let toggleTag   = tagMaker('button',  '',     'id= "tog" type="button" value="" ', 
+//   'width:15vw; height:10vh; background:darkgray; font; font-size: 7vh;');
+// //cont
+// let cont        = tagMaker('div',     toggleTag+searchInput+submitBnt, '', 
+//   'display:flex; width:70vw; height:10vh; background:black;');
+// // bigcont is body cont
+// let bigCont     = tagMaker('div',     cont,    '', 
+//   arrange('center', 'center')+'width:100vw; height:100vh; background:black;');
+//   // doc
+// let mainDoc = docMaker('main', styleGray(), bigCont+toggleScript);
 
 
+rootRoute( contenType, toggleMaker, checkreq, arrange )
 
 // make server
 let serv = http.createServer((req,res)=> {
@@ -44,8 +46,6 @@ let serv = http.createServer((req,res)=> {
     }
   });
   if(req.method === 'GET' && req.url === '/'){
-    // console.log(req.method);
-    // console.log(req.url);
     checkreq(req.method, req.url);
     res.writeHead(200, contHtml); 
     res.end(mainDoc);
