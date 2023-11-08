c31d25d0f971c6bab7c05b6e842891653ea36e6f
csm <csm009177@gmail.com>
2023-11-08 14:14:02 +0900

    make rootRoute

--- app.js
+++ app.js
@@ -2,14 +2,13 @@
 import http from 'http';
 import fs from 'fs';
 import { contenType } from './mod/contenType.mjs';
-import { entryPoint } from './routes/route.mjs';
+import { entryPoint } from './routes/rootRoute.mjs';
 
 // declare port
 let port = 3217;
 
 let serv = http.createServer((req, res)=> {
   entryPoint(req, res, contenType)
-    
 })
 
 serv.listen(port, ()=> {
