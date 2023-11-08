c6a5bb5fd7e6099d97b94dff1bce6eed2bc8d77b
csm <csm009177@gmail.com>
2023-11-08 11:00:14 +0900

    remake server

--- app.js
+++ app.js
@@ -2,39 +2,12 @@
 const http = require('http');
 const fs = require('fs');
 // custom module
-const contenType = require('./mod/contenType');
-const checkreq = require('./mod/checkreq');
-const docMaker = require('./doc/docMaker');
-const tagMaker = require('./mod/tagMaker');
-const { styleWhite, styleDark, styleGray } = require('./mod/asteriskStyle');
-const arrange = require('./mod/contentArrange');
-const toggleMaker = require('./mod/toggleMaker');
-const rootRoute = require('./route/route');
+const contenType = require('./mod/contenType')
+let contenTypeHtml = contenType("text/html", 'utf8')
 
+const docMaker = require('./doc/docMaker')
+let maindoc = docMaker('main', "width:100vw; height:100vh" , 'test')
 
-// custom module use
-// let contHtml = contenType('text/html','utf8');
-// // submit Bnt
-// let submitBnt   = tagMaker('button',  'search',  '', 
-//   'width:15vw; height:10vh; background:darkgray; font-size: 7vh;');
-// // input
-// let searchInput = tagMaker('input',   '',     'type="text"', 
-// 'width:40vw; height:10vh; background:white;');
-// // toggle tag
-// let toggleScript = toggleMaker('tog', 'User', 'green', 'Expert', 'red');
-// let toggleTag   = tagMaker('button',  '',     'id= "tog" type="button" value="" ', 
-//   'width:15vw; height:10vh; background:darkgray; font; font-size: 7vh;');
-// //cont
-// let cont        = tagMaker('div',     toggleTag+searchInput+submitBnt, '', 
-//   'display:flex; width:70vw; height:10vh; background:black;');
-// // bigcont is body cont
-// let bigCont     = tagMaker('div',     cont,    '', 
-//   arrange('center', 'center')+'width:100vw; height:100vh; background:black;');
-//   // doc
-// let mainDoc = docMaker('main', styleGray(), bigCont+toggleScript);
-
-
-rootRoute( contenType, toggleMaker, checkreq, arrange )
 
 // make server
 let serv = http.createServer((req,res)=> {
@@ -47,8 +20,8 @@ let serv = http.createServer((req,res)=> {
   });
   if(req.method === 'GET' && req.url === '/'){
     checkreq(req.method, req.url);
-    res.writeHead(200, contHtml); 
-    res.end(mainDoc);
+    res.writeHead(200, contenTypeHtml); 
+    res.end(maindoc);
   } 
 })
 
