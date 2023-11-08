7a6d4716d5b78187feab0a9ddf0a7a7f5c50628a
csm <csm009177@gmail.com>
2023-11-08 11:55:03 +0900

    want make checkServerError Module

--- app.js
+++ app.js
@@ -1,48 +1,12 @@
 // http, fs
 const http = require('http');
 const fs = require('fs');
-// custom module
-const contenType = require('./mod/contenType');
-const checkreq = require('./mod/checkreq');
-const docMaker = require('./doc/docMaker');
-const tagMaker = require('./mod/tagMaker');
-const { styleWhite, styleDark, styleGray } = require('./mod/asteriskStyle');
-const arrange = require('./mod/contentArrange');
-const toggleMaker = require('./mod/toggleMaker');
+// port
+let port =3217;
 
-
-// custom module use
-let contHtml = contenType('text/html','utf8');
-// submit Bnt
-let submitBnt   = tagMaker('button',  'search',  '', 
-  'width:15vw; height:10vh; background:darkgray; font-size: 7vh;');
-// input
-let searchInput = tagMaker('input',   '',     'type="text"', 
-'width:40vw; height:10vh; background:white;');
-// toggle tag
-let toggleScript = toggleMaker('tog', 'User', 'green', 'Expert', 'red');
-let toggleTag   = tagMaker('button',  '',     'id= "tog" type="button" value="" ', 
-  'width:15vw; height:10vh; background:darkgray; font; font-size: 7vh;');
-//cont
-let cont        = tagMaker('div',     toggleTag+searchInput+submitBnt, '', 
-  'display:flex; width:70vw; height:10vh; background:black;');
-// bigcont is body cont
-let bigCont     = tagMaker('div',     cont,    '', 
-  arrange('center', 'center')+'width:100vw; height:100vh; background:black;');
-  // doc
-let mainDoc = docMaker('main', styleGray(), bigCont+toggleScript);
-
-
-
+CheckServerError(port, serv)
 // make server
 let serv = http.createServer((req,res)=> {
-  serv.on('error', (error) => {
-    if (error.code === 'EADDRINUSE') {
-      console.error('포트 ' + port + '은 이미 사용 중입니다. 다른 포트를 시도하세요.');
-    } else {
-      console.error('서버에서 오류 발생: ' + error.message);
-    }
-  });
   if(req.method === 'GET' && req.url === '/'){
     // console.log(req.method);
     // console.log(req.url);
@@ -52,9 +16,6 @@ let serv = http.createServer((req,res)=> {
   } 
 })
 
-
-// port
-let port =3217;
 // server listen
 serv.listen(port, () => {
   console.log(`
--- mod/contentArrange.js
+++
@@ -1,4 +0,0 @@
-function arrange(align, justify){
-  return `display: flex; flex-wrap: wrap; align-content: ${align}; justify-content: ${justify};`;
-}
-module.exports = arrange;
