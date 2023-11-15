ae8406262a39491e7c678944ddd8ccb1b7b68ba0
csm <csm009177@gmail.com>
2023-11-15 12:57:13 +0900

    make estimate button

--- routes/rootRoute.js
+++ routes/rootRoute.js
@@ -1,9 +1,11 @@
 // routes/routes.js
 import express from 'express';
+import fs, { readFile } from 'fs';
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
+  fs.readFile('../public/index.html')
 });
 
 router.post('/login', (req, res) => {
