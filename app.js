027edb48782b92ee14ce7042ed99772ed28497d9
csm <csm009177@gmail.com>
2023-11-15 11:42:17 +0900

    app.js - serv, GET, POST

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-console.log('hallot')
+// import express
+import express from 'express'
+const app = express();
+// declare port
+const port = 3217;
+
+
+app.use(express.static('public')); // 정적 파일 서비스
+
+app.get('/login', (req,res)=> {
+  
+})
+
+app.listen(port, ()=> {
+  console.log(`
+  http://localhost:${port}
+  `)
+})
