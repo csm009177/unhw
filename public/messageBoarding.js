73131933f588223253428b41f566f53601bade69
HongMoonGI <hmoongi94@gmail.com>
2023-11-17 18:16:12 +0900

    성민이형 작업을 하고있는데 아직 작동이안돼요.일단해놓은것은올려놓을게요.주말에 더작업해볼께요

--- public/messageBoarding.js
+++ public/messageBoarding.js
@@ -1,10 +1,10 @@
-document.addEventListener("DOMContentLoaded", () =>{
+document.addEventListener("DOMContentLoaded", () => {
   const submit = document.getElementById("submit")
   const boarding = document.getElementById("boarding")
   const inputBox = document.getElementById("inputBox")
   boarding.style.alignContent = "left"
   submit.addEventListener("click", function () {
-  const message = inputBox.value;
+    const message = inputBox.value;
     if (message.trim() !== "") {
       const messageElement = document.createElement("div");
       messageElement.textContent = `user:${message}`;
@@ -12,6 +12,19 @@ document.addEventListener("DOMContentLoaded", () =>{
       inputBox.value = "";
       // 데이터를 서버에 전송
       // sendDataToServer(message);
+      fetch('/savedata', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ data: message })
+      })
+        .then(response => {
+          if (response.ok) {
+            console.log('data를 서버로 보내는데 성공')
+          }
+        })
+
     }
   })
 })
