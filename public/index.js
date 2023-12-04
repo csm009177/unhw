95cbf9bfd72c6b6fc05232cb015cd9a76fad0f42
csm <csm009177@gmail.com>
2023-12-04 14:16:34 +0900

    custom component

--- public/index.js
+++ public/index.js
@@ -2,16 +2,18 @@ export function createElement(type, props, ...children) {
     return { type, props, children};
   }
 
-export function component(stateData) {
+export function component(stateData, contId) {
     const menuItems = [];
     for (let i=0; i<stateData.length; i++){
       const item = stateData[i];
-      const menuItem = createElement('li', {}, createElement('a', {href:item.hash}, item.text));
+      const menuItem = createElement('li', {style:"list-style: none;"}, createElement('a', {href:item.hash}, item.text));
       menuItems.push(menuItem);
     }
-    const menu = createElement('ul', {}, ...menuItems);
-    return createElement('div', {}, menu);
+    return createElement('div', { 
+      style:"display: flex; text-align: center; flex-direction: column; align-items: center;"}, 
+      ...menuItems);
   }
+  
 export function render(virtualDom) {
     if(typeof virtualDom === 'string') {
       return document.createTextNode(virtualDom);
@@ -29,18 +31,3 @@ export function render(virtualDom) {
     return element;
   }
 
-  const stateData = [
-    {hash : '#home', text:'home'},
-    {hash : '#cpu', text:'cpu'},
-    {hash : '#mainboard', text:'mainboard'},
-    {hash : '#power', text:'power'},
-    {hash : '#ram', text:'ram'},
-    {hash : '#ssd', text:'ssd'},
-    {hash : '#odd', text:'odd'},
-    {hash : '#hdd', text:'hdd'},
-    {hash : '#gpu', text:'gpu'},
-  ];
-
-  const virtualDom = component(stateData);
-  const board = document.getElementById('board');
-  board.appendChild(render(virtualDom));
