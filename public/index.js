549f791e17c99f1b2ae2cfe0cb8eb2bb07cd466f
csm <csm009177@gmail.com>
2023-12-05 08:42:40 +0900

    use width %

--- public/index.js
+++ public/index.js
@@ -2,15 +2,15 @@ export function createElement(type, props, ...children) {
     return { type, props, children};
   }
 
-export function component(stateData, contId) {
+export function component(stateData, direction) {
     const menuItems = [];
     for (let i=0; i<stateData.length; i++){
       const item = stateData[i];
-      const menuItem = createElement('li', {style:"list-style: none;"}, createElement('a', {href:item.hash}, item.text));
+      const menuItem = createElement('li', {style:"list-style: none; width:100%;"}, createElement('a', {href:item.hash}, item.text));
       menuItems.push(menuItem);
     }
     return createElement('div', { 
-      style:"display: flex; text-align: center; flex-direction: column; align-items: center;"}, 
+      style:`display: flex; text-align: center; flex-direction: ${direction}; align-items: center;`}, 
       ...menuItems);
   }
   
