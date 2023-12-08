7aaa71e72f34590bdbf87159c0e088a913088a42
csm <csm009177@gmail.com>
2023-12-08 18:06:16 +0900

    Remove styles and move them to inde.css

--- public/module/component.js
+++ public/module/component.js
@@ -1,15 +1,20 @@
 
-import {component, render} from "./module/component.js"; // component.js 에서 component()와 render() 가져오기
-import {memoProcess} from "./module/memoProcess.js";     // memoProcess.js 에서 memoProcess() 가져오기
+import { createElement, component, render } from "./component"; // component.js 에서
 import {hashChanger} from "./module/hashChanger.js";     // hashChanger.js 에서 hashChanger() 가져오기
 
-const headstateData = [
-  {hash: "#home", text: "Home", name: "Home"},           // memo 기능 
-  {hash: "#team", text: "Team", name: "Team"},           // team 소개 
-  {hash: "#member", text: "Member", name: "Member"},     // member 소개 
-  {hash: "#purpose", text: "Purpose", name: "Purpose"},  // purpose 소개 
+const stateData = [
+  { hash: '#home', text: 'home' },
+  { hash: '#cpu', text: 'cpu' },
+  { hash: '#m/b', text: 'm/b' },
+  { hash: '#p/w', text: 'p/w'},
+  { hash: '#ram', text: 'ram' },
+  { hash: '#ssd', text: 'ssd' },
+  { hash: '#odd', text: 'odd' },
+  { hash: '#hdd', text: 'hdd' },
+  { hash: '#gpu', text: 'gpu' },
 ];
+
 // append
 const headvirtualDom = component(headstateData); // 상태데이터로 컴포넌트 만들어서 dom에 넣기
-const headcontainer = document.getElementById("head"); // 어펜드할 대상 지정
-headcontainer.appendChild(render(headvirtualDom)); // dom을 렌더해서 대상에 어펜드 하기
+const root = document.getElementById("root"); // 어펜드할 대상 지정
+root.appendChild(render(headvirtualDom)); // dom을 렌더해서 대상에 어펜드 하기
