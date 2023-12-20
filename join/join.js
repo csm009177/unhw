4d3f2a35e9a2852e1812b4c7f26bdea0b1c7989e
csm <csm009177@gmail.com>
2023-12-20 14:28:22 +0900

    make get request

--- join/join.js
+++ join/join.js
@@ -2,25 +2,26 @@
 import http, { METHODS } from 'http';
 import fs from 'fs';
 import path from 'path';
+const joinformPath = './join.html'
 
-// Create a server
-const server = http.createServer((req, res) => {
-    // Set static file serving logic
-    if (req.url === '/' || req.url === '/join.html') {
-        fs.readFile(path.join('./join.html'), (err, content) => {
-            if (err) throw err;
+let serv = http.createServer((req, res) => {
+    if (req.method === 'GET' && req.url === '/') {
+        // Read the join.html file
+        fs.readFile(path.join(__dirname, joinformPath), 'utf8', (err, data) => {
+            if (err) {
+                res.writeHead(500, { 'Content-Type': 'text/plain' });
+                res.end('Internal Server Error');
+                return;
+            }
+
+            // Set response header and send the file content
             res.writeHead(200, { 'Content-Type': 'text/html' });
-            res.end(content);
-        });
-    } else if (req.url === '/join.js') {
-        fs.readFile(path.join('./join.js'), (err, content) => {
-            if (err) throw err;
-            res.writeHead(200, { 'Content-Type': 'application/javascript' });
-            res.end(content);
+            res.end(data);
         });
     } else {
-        res.writeHead(404, { 'Content-Type': 'text/html' });
-        res.end('<h1>404 Not Found</h1>');
+        // Handle other routes or methods (here, just a basic 404 response)
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end('Not Found');
     }
 });
 
@@ -28,6 +29,6 @@ const server = http.createServer((req, res) => {
 const port = 3217;
 
 // Start the server
-server.listen(port, () => {
+serv.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
