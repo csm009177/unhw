d1c6375977d3be06b68f3baac5bbccddf7ac124a
csm <csm009177@gmail.com>
2023-12-21 21:40:26 +0900

    test.html is about make component

--- test.js
+++ test.js
@@ -7,6 +7,18 @@ function makeCompo(type, attributes, children){
   return elementString; 
 }
 
-const resultValue= makeCompo("h1", `id="example" class="active" style: "colcor:salmon;" `, "최성민")
-console.log(resultValue)
+// const resultValue= makeCompo("h1", `id="example" class="active" style: "colcor:salmon;" `, "최성민")
+// console.log(resultValue)
 
+function makeCompo2(type, attributes, children){
+  const elementString = `<${type}`
+  for(let key in attributes) {
+    elementString = elementString + ` ${key} = '${attributes[key]}'`;
+    elementString = elementString +'>';
+  }
+  elementString += `</${type}>`;
+  return elementString; 
+}
+const resultValue= makeCompo2("h1", {style:'color:blue'}, "최성민")
+const parsedresult = parseInt(resultValue)
+console.log(parsedresult)
