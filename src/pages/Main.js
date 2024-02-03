20b197b67a5344d2e29f26c68c6fdba0dc4c0ca0
csm <csm009177@gmail.com>
2024-02-04 04:06:03 +0900

    메인페이지 토글 리스크 박스 만들기

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -8,18 +8,23 @@ function Main() {
     setIsOpen(!isOpen);
   };
 
+  const createButton = () => {
+    
+  }
+
   return (
     <div className="Main">
       {isOpen && (
         <div className="Left-var">
           <ul>
+            <p>unhw</p>
             <li>Item 1</li>
             <li>Item 2</li>
             <li>Item 3</li>
           </ul>
         </div>
       )}
-      <button onClick={toggleList}>{isOpen ? '<<' : '>>'}</button>
+      <button onClick={toggleList}>{isOpen ? '◀' : '▶'}</button>
     </div>
   );
 }
