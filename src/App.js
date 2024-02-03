e41b4c0a7b762a90e5e81c501f9457a34bd429df
csm <csm009177@gmail.com>
2024-02-04 00:52:29 +0900

    SignUp 스타일 작성

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import { useEffect, useState } from 'react';
-import './App.css';
-import Lobby from './Lobby';
-import Main from './Main';
+import { useEffect, useState } from "react";
+import "./App.css";
+import Lobby from "./Lobby";
+import Main from "./Main";
+import Login from "./ui/login";
+import SignUp from "./ui/signup";
 
 function App() {
   const [url, setUrl] = useState(window.location.pathname); // 현재 URL 경로를 가져옴
 
   useEffect(() => {
     // URL 경로가 "/"일 때만 setUrl 함수를 호출하여 상태를 변경
-    if (window.location.pathname === "/login") {
-      setUrl("/login");
+    if (window.location.pathname === "/lobby") {
+      setUrl("/lobby");
     }
   }, []); // 의존성 배열을 비워두어 한 번만 실행되도록 설정
 
   return (
     <div className="App">
-      {/* url이 "/"인 경우에만 LoginForm 컴포넌트를 렌더링 */}
       {url === "/lobby" && <Lobby />}
-      {url === "/" && <Main/>}
+      {url === "/" && <Main />}
+      {url === "/login" && <Login />}
+      {url === "/signup" && <SignUp />}
     </div>
   );
 }
--- src/ui/login.js
+++ src/ui/login.js
@@ -1,10 +1,12 @@
-import './App.css';
+import "../App.css";
 
 function Login() {
   return (
-    <div className="Login">
-      Main Page
-    </div>
+    <form className="Login">
+      <input placeholder="type ID here" style={{marginTop:"auto"}}></input>
+      <input placeholder="type PW here"></input>
+      <button style={{color:"green"}}>submit</button>
+    </form>
   );
 }
 
