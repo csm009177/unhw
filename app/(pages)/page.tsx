12119aa074f0a0249d1ed6e6e82866f1cf6134eb
csm <csm009177@gmail.com>
2024-02-06 16:37:40 +0900

    로테이션 멘트 컴포넌트화

--- app/(pages)/page.tsx
+++ app/(pages)/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import React, { useState, useEffect } from "react";
-import "./globals.css";
+import "../globals.css";
 
 export default function Home() {
   const [seconds, setSeconds] = useState(0);
