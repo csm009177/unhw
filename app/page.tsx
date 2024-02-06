a511042bda3f5ba271c9a3420b4df441e56a9007
csm <csm009177@gmail.com>
2024-02-06 16:09:40 +0900

    메인 페이지 로테이션 멘트

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,34 @@
-import Image from "next/image";
+'use client'
+
+import React, { useState, useEffect } from "react";
+import "./globals.css";
 
 export default function Home() {
+  const [seconds, setSeconds] = useState(0);
+  const [comments, setComments] = useState([
+    "1번 멘트",
+    "2번 멘트",
+    "3번 멘트",
+    "4번 멘트",
+    "5번 멘트"
+  ]);
+  const [currentCommentIndex, setCurrentCommentIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setSeconds(prevSeconds => prevSeconds + 1);
+      setCurrentCommentIndex(prevIndex => (prevIndex + 1) % comments.length);
+    }, 500);
+
+    return () => clearInterval(interval);
+  }, [comments.length]);
+
   return (
-    <main className="App">
-      
-    </main>
+    <div className="Lobby">
+      <div className="Lobby-LeftCont">{comments[currentCommentIndex]}</div>
+      <div className="Lobby-RightCont">right</div>
+    </div>
   );
 }
+
+
