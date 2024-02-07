3a7182d23a116d5b5d0c4a73cf854e66a39fe613
csm <csm009177@gmail.com>
2024-02-07 10:46:29 +0900

    useContext로 메인페이지 하위 컴포넌트 연결 완료

--- app/ui/ToggleLeftVar.tsx
+++ app/ui/ToggleLeftVar.tsx
@@ -1,28 +1,32 @@
 'use client'
-// ToggleLeftVar 컴포넌트
-import React, { useContext, useEffect, useState } from "react";
+
+import { useContext, useEffect, useState } from "react";
 import "../globals.css";
 import AddItemList from './AddItemList';
-import { openContext } from "../context/styleContext";
+import {openContext,selectContext } from "../context/styleContext";
 
 export default function ToggleLeftVar() {
-  const [url, setUrl] = useState(window.location.pathname);
+  const [url, setUrl] = useState(window.location.pathname); // 현재 URL 경로를 가져옴
+  const [items, setItems] = useState([]);
   const { isOpen, setIsOpen } = useContext(openContext);
+  const {selectedItemIndex, setSelectedItemIndex } = useContext(selectContext)
 
   const toggleList = () => {
     setIsOpen(!isOpen);
   };
 
   useEffect(() => {
+    // 로그인 토큰이 없으면 "/lobby"로 URL 경로 설정
     const token = localStorage.getItem("token");
     if (!token) {
       setUrl("/lobby");
     }
-  }, []);
+  }, []); // 한 번만 실행되도록 설정
 
   return (
-    <div style={{display:"flex", flexDirection:"row", background:"#434343", height:"100%"}}>
-      <AddItemList />
+    <div 
+      style={{display:"flex", flexDirection:"row", background:"#434343", height:"100%"}}>
+        <AddItemList isOpen={isOpen} setIsOpen={setIsOpen}/>
       <button onClick={toggleList} style={{backgroundColor: "#434343", fontSize:"2vw"}}>{isOpen ? "◀" : "▶"}</button>
     </div>
   );
