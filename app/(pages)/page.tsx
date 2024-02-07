2a28854d1a4d4249f422034da54e2865896a3f28
csm <csm009177@gmail.com>
2024-02-07 10:46:04 +0900

    useContext로 메인페이지 하위 컴포넌트 연결 완료

--- app/(pages)/page.tsx
+++ app/(pages)/page.tsx
@@ -1,51 +1,47 @@
 "use client";
 
-import React, { useState, useEffect, createContext, useContext } from "react";
+// Main 컴포넌트
+import React, { useState } from "react";
 import "../globals.css";
 import ToggleLeftVar from "../ui/ToggleLeftVar";
 import { openContext, selectContext } from "../context/styleContext";
 
 export default function Main() {
   const [isOpen, setIsOpen] = useState(true);
-  const [selectedItemIndex, setSelectedItemIndex ] = useState(selectContext)
-
-  const handleItemClick = (index) => {
-    setSelectedItemIndex(index);
-    // console.log(selectedItemIndex);
-  };
+  const [selectedItemIndex, setSelectedItemIndex ] = useState(null);
 
   return (
     <selectContext.Provider value={{selectedItemIndex,setSelectedItemIndex }}>
-    <openContext.Provider value={{ isOpen, setIsOpen }}>
-      <div className="Main">
-        <div
-          className="Main-LeftVar"
-          style={{
-            width: isOpen ? "20%" : "2%",
-            height: "100%",
-            backgroundColor: "#434343",
-            overflow: "hidden",
-          }}
-        >
-          <ToggleLeftVar />
+      <openContext.Provider value={{ isOpen, setIsOpen }}>
+        <div className="Main">
+          <div
+            className="Main-LeftVar"
+            style={{
+              width: isOpen ? "20%" : "2%",
+              height: "100%",
+              backgroundColor: "#434343",
+              overflow: "hidden",
+            }}
+          >
+            <ToggleLeftVar />
+          </div>
+          <div
+            className="Main-RightCont"
+            style={{
+              width: isOpen ? "80%" : "98%",
+              height: "100%",
+              backgroundColor: "#373737",
+              overflow: "hidden",
+            }}
+          >
+            {selectedItemIndex !== null && (
+              <div>
+                <p>Selected Item : {selectedItemIndex}</p>
+              </div>
+            )}
+          </div>
         </div>
-        <div
-          className="Main-RightCont"
-          style={{
-            width: isOpen ? "80%" : "98%",
-            height: "100%",
-            backgroundColor: "#373737",
-            overflow: "hidden",
-          }}
-        >
-          {selectedItemIndex !== null && (
-            <div>
-              <p>Selected Item : {selectedItemIndex}</p>
-            </div>
-          )}
-        </div>
-      </div>
-    </openContext.Provider>
+      </openContext.Provider>
     </selectContext.Provider>
   );
 }
--- app/ui/ToggleLeftVar.tsx
+++ app/ui/ToggleLeftVar.tsx
@@ -1,30 +1,28 @@
-import { useContext, useEffect, useState } from "react";
+'use client'
+// ToggleLeftVar 컴포넌트
+import React, { useContext, useEffect, useState } from "react";
 import "../globals.css";
 import AddItemList from './AddItemList';
-import {openContext,selectContext } from "../context/styleContext";
+import { openContext } from "../context/styleContext";
 
 export default function ToggleLeftVar() {
-  const [url, setUrl] = useState(window.location.pathname); // 현재 URL 경로를 가져옴
-  const [items, setItems] = useState([]);
+  const [url, setUrl] = useState(window.location.pathname);
   const { isOpen, setIsOpen } = useContext(openContext);
-  const {selectedItemIndex, setSelectedItemIndex } = useContext(selectContext)
 
   const toggleList = () => {
     setIsOpen(!isOpen);
   };
 
   useEffect(() => {
-    // 로그인 토큰이 없으면 "/lobby"로 URL 경로 설정
     const token = localStorage.getItem("token");
     if (!token) {
       setUrl("/lobby");
     }
-  }, []); // 한 번만 실행되도록 설정
+  }, []);
 
   return (
-    <div 
-      style={{display:"flex", flexDirection:"row", background:"#434343", height:"100%"}}>
-        <AddItemList isOpen={isOpen} setIsOpen={setIsOpen}/>
+    <div style={{display:"flex", flexDirection:"row", background:"#434343", height:"100%"}}>
+      <AddItemList />
       <button onClick={toggleList} style={{backgroundColor: "#434343", fontSize:"2vw"}}>{isOpen ? "◀" : "▶"}</button>
     </div>
   );
