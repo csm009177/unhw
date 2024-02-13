3f112a37df44bc2f6359b412a87f91abc0e63675
csm <csm009177@gmail.com>
2024-02-13 16:35:14 +0900

    메인 페이지 표현 로직 수정

--- app/(pages)/layout.tsx
+++ app/(pages)/layout.tsx
@@ -10,17 +10,38 @@ import { selectContext, openContext } from "../context/styleContext";
 import { devNull } from "os";
 
 export default function MainLayout({ children }: ChildrenProps) {
-  const [showLobby, setShowLobby] = useState(true); // 변수명 수정
   const pathname = usePathname();
+  const router = useRouter()
+  const [showLeft, setShowLeft] = useState(true); // 변수명 수정
+  const [showChild, setshowChild] = useState(true); // 변수명 수정
+  const [showLobby, setShowLobby] = useState(false); // 변수명 수정
   const [isOpen, setIsOpen] = useState(true);
   const [selectedItemIndex, setSelectedItemIndex] = useState(null);
 
   useEffect(() => {
     const token = localStorage.getItem("jwtToken");
-    if (!token || pathname !== "/") {
-      setShowLobby(true); // 작업을 위해 임시로 true 해놓음
+    // 토큰이 없는 경우
+    if ( !token ) {
+      if (pathname === "/lobby") {
+        setShowLeft(false);
+        setShowLobby(true);
+        setshowChild(false);
+      }
+      if(pathname === "/login" || pathname === "/signup"){
+        setShowLeft(false);
+        setShowLobby(false);
+        setshowChild(true);
+      }
+      if(pathname !== "/lobby" && pathname !== "/login" && pathname !== "/signup"){
+        router.push("/lobby")
+      }
+    // 토큰이 있는 경우
     } else {
-      setShowLobby(false); // 작업을 위해 임시로 false 해놓음
+      if(pathname === "/"){
+        setShowLeft(true);
+        setShowLobby(false);
+        setshowChild(true);
+      }
     }
   }, [pathname]);
 
@@ -30,8 +51,9 @@ export default function MainLayout({ children }: ChildrenProps) {
         value={{ selectedItemIndex, setSelectedItemIndex }}
       >
         <openContext.Provider value={{ isOpen, setIsOpen }}>
-          {showLobby ? <ToggleLeftVar /> : null}
-          {showLobby ? children : <Lobby />} {/* 조건부 렌더링 */}
+          {showLeft ? <ToggleLeftVar /> : null}
+          {showLobby ? <Lobby /> : null} {/* 조건부 렌더링 */}
+          {showChild ? children : null}
         </openContext.Provider>
       </selectContext.Provider>
     </div>
