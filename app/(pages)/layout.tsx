2e6d1530b83af47d295ba6573d10c8dd7df8d651
csm <csm009177@gmail.com>
2024-02-13 17:06:26 +0900

    로그인 후 메인페이지로 리디렉션

--- app/(pages)/layout.tsx
+++ app/(pages)/layout.tsx
@@ -2,47 +2,37 @@
 import "../globals.css";
 import { ChildrenProps } from "./ChildrenProps";
 import { useEffect, useState } from "react";
-import { usePathname, useSearchParams } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 import Lobby from "../ui/Lobby";
-import { useRouter } from "next/navigation";
 import ToggleLeftVar from "../ui/ToggleLeftVar";
 import { selectContext, openContext } from "../context/styleContext";
-import { devNull } from "os";
 
 export default function MainLayout({ children }: ChildrenProps) {
   const pathname = usePathname();
-  const router = useRouter()
-  const [showLeft, setShowLeft] = useState(true); // 변수명 수정
-  const [showChild, setshowChild] = useState(true); // 변수명 수정
-  const [showLobby, setShowLobby] = useState(false); // 변수명 수정
+  const router = useRouter();
+  const [showLeft, setShowLeft] = useState(true);
+  const [showChild, setshowChild] = useState(true);
+  const [showLobby, setShowLobby] = useState(false);
   const [isOpen, setIsOpen] = useState(true);
   const [selectedItemIndex, setSelectedItemIndex] = useState(null);
 
   useEffect(() => {
     const token = localStorage.getItem("jwtToken");
-    // 토큰이 없는 경우
-    if ( !token ) {
-      if (pathname === "/lobby") {
-        setShowLeft(false);
-        setShowLobby(true);
-        setshowChild(false);
-      }
-      if(pathname === "/login" || pathname === "/signup"){
-        setShowLeft(false);
-        setShowLobby(false);
-        setshowChild(true);
-      }
-      if(pathname !== "/lobby" && pathname !== "/login" && pathname !== "/signup"){
-        router.push("/lobby")
-      }
-    // 토큰이 있는 경우
-    } else {
-      if(pathname === "/"){
-        setShowLeft(true);
-        setShowLobby(false);
-        setshowChild(true);
-      }
-    }
+    if (token && pathname === "/") {
+      setShowLeft(true);
+      setShowLobby(false);
+      setshowChild(true);
+    } else if (!token && pathname === "/lobby") {
+      setShowLeft(false);
+      setShowLobby(true);
+      setshowChild(false);
+    } else if ((!token && pathname === "/login") || pathname === "/signup") {
+      setShowLeft(false);
+      setShowLobby(false);
+      setshowChild(true);
+    } else if (!token && pathname !== "/lobby" && pathname !== "/login" && pathname !== "/signup") {
+      router.replace("/lobby"); // push 대신 replace 사용하여 이동
+    } 
   }, [pathname]);
 
   return (
@@ -52,7 +42,7 @@ export default function MainLayout({ children }: ChildrenProps) {
       >
         <openContext.Provider value={{ isOpen, setIsOpen }}>
           {showLeft ? <ToggleLeftVar /> : null}
-          {showLobby ? <Lobby /> : null} {/* 조건부 렌더링 */}
+          {showLobby ? <Lobby /> : null}
           {showChild ? children : null}
         </openContext.Provider>
       </selectContext.Provider>
