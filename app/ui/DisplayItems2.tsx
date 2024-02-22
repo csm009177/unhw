0c4a500bc0f3be19eafadd2b63f4ea5e05551f6a
csm <csm009177@gmail.com>
2024-02-22 18:15:13 +0900

    type별 버튼 생성

--- app/ui/DisplayItems2.tsx
+++ app/ui/DisplayItems2.tsx
@@ -1,3 +1,4 @@
+"use client";
 // 클라이언트 측 코드입니다. 사용자가 입력한 검색어로 아이템을 검색하고 표시합니다.
 
 import React, { useEffect, useState } from "react";
@@ -9,7 +10,19 @@ export default function DisplayItems() {
 
   const [types, setTypes] = useState([]);
   const [selectedTypes, setSelectedTypes] = useState([]);
+
   
+  // 아이템 유형을 가져오는 비동기 함수입니다.
+  const fetchTypes = async () => {
+    try {
+      const response = await fetch("/fetchTypes");
+      const data = await response.json();
+      setTypes(data.types);
+    } catch (error) {
+      console.error("Error fetching types:", error);
+    }
+  };
+
   // 검색을 처리하는 함수입니다.
   const handleSearch = async (input) => {
     try {
@@ -37,30 +50,26 @@ export default function DisplayItems() {
     setSearchResults(searchResults); // 검색 결과를 업데이트합니다. (이 부분은 필요하지 않아 보입니다.)
   };
 
-  const handleTypeFilter = async () => {
-
-  }
-
+  // 컴포넌트가 처음 마운트될 때 아이템 유형을 가져오는 함수를 호출합니다.
   useEffect(() => {
-    // 컴포넌트가 처음 마운트될 때 아이템 유형을 가져오는 함수를 호출합니다.
-    handleInputChange
-    handleResultChange
-    // handleTypeFilter
+    fetchTypes();
+    handleInputChange;
+    handleResultChange;
+    handleSearch
   }, []);
 
   return (
-    <div style={{ display: "flex", flexDirection:"column" }}>
-      <input
-        type="text"
-        style={{ color: "black" }}
-        value={inputValue}
-        onChange={handleInputChange}
-      />
-      <button onClick={() => handleSearch(inputValue)}>검색</button> {/* 검색 버튼 */}
-
-      {/* {searchResults.map(type => (
-        <button key={type} onClick={handleTypeFilter}>{type}</button>
-      ))} */}
+    <div style={{ display: "flex", flexDirection: "column" }}>
+      <input type="text" style={{ color: "black" }} value={inputValue} onChange={handleInputChange} />
+        {/* 검색 제출 버튼 */}
+      <button onClick={() => handleSearch(inputValue)}>검색</button>
+        {/* 타입 검색 버튼 */}
+        <div style={{ display: "flex", flexDirection: "row", justifyContent: "space-between" }}>
+        {types.map((type) => (
+          <button key={type} onClick={()=> setSelectedTypes(type)}>{type}</button>
+        ))}
+        </div>
+        {/* 검색 결과 출력 */}
       <div onChange={handleResultChange}>
         {searchResults.map((item) => (
           <li key={item.id}>
