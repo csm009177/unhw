1bdb2105a289f8dadbd42207eb7db6a7471a24b3
csm <csm009177@gmail.com>
2024-02-25 18:21:25 +0900

    디스플레이 컴포넌트 모델 데이터 불러오기

--- app/ui/DisplayItems.tsx
+++ app/ui/DisplayItems.tsx
@@ -1,50 +1,103 @@
 "use client";
 
-import React, { useContext, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 
 export default function DisplayItems() {
-  const [types, setTypes] = useState([]);
-  const [selectedType, setSelectedType] = useState([]);
+  const [types, setType] = useState([]);
+  const [selectedType, setSelectedType] = useState("");
 
-  const [brands, setBrands] = useState([]);
-  const [selectedBrands, setSelectedBrands] = useState([]);
+  const [brands, setBrand] = useState([]);
+  const [selectedBrand, setSelectedBrand] = useState("");
 
-  const [result, setResult] = useState("");
+  const [model, setModel] = useState([]);
+  const [selectedModel, setSelectedModel] = useState("");
 
   // 처음 랜더링될때 아이템 유형을 가져오는 비동기 함수입니다.
   const fetchTypes = async () => {
     try {
       const response = await fetch("/fetchTypes");
       const data = await response.json();
-      setTypes(data.types);
+      setType(data.types);
     } catch (error) {
       console.error("Error fetching types:", error);
     }
   };
 
   // 선택된 아이템 유형에 따라 브랜드를 가져오는 비동기 함수입니다.
-  const fetchBrands = async (selectedType) => {
+  const fetchBrand = async (selectedType) => {
     try {
       const response = await fetch(`/fetchBrands?type=${selectedType}`);
       const data = await response.json();
-      setBrands(data.brands);
+      setBrand(data.brands);
     } catch (error) {
       console.error("Error fetching brands:", error);
     }
   };
+
+  // 선택된 아이템 유형과 브랜드에 따라 모델을 가져오는 비동기 함수입니다.
+  const fetchModel = async (selectedType, selectedBrand) => {
+    try {
+      const response = await fetch(
+        `/fetchModels?type=${selectedType}&brand=${selectedBrand}`
+      );
+      const data = await response.json();
+      setModel(data.models);
+    } catch (error) {
+      console.error("Error fetching models:", error);
+    }
+  };
+
   // 컴포넌트가 처음 마운트될 때 아이템 유형을 가져오는 함수를 호출합니다.
   useEffect(() => {
     fetchTypes();
-    console.log(selectedType)
+  }, []);
+
+  // 선택된 아이템 유형이 변경될 때마다 해당 유형에 대한 브랜드를 가져오도록 합니다.
+  useEffect(() => {
+    if (selectedType !== "") {
+      fetchBrand(selectedType);
+    }
   }, [selectedType]);
+
+  // 선택된 브랜드가 변경될 때마다 해당 브랜드에 대한 모델을 가져오도록 합니다.
+  useEffect(() => {
+    if (selectedBrand !== "") {
+      fetchModel(selectedType, selectedBrand);
+    }
+  }, [selectedBrand]);
 
   return (
     <>
-      {types.map((type) => (
-        <button key={type} onClick={() => setSelectedType(type)}>
-          {type}
-        </button>
-      ))}
+      {/* 아이템 유형 버튼들 */}
+      <div>
+        {types.map((type) => (
+          <button key={type} onClick={() => setSelectedType(type)}>
+            {type}
+          </button>
+        ))}
+      </div>
+      {/* 브랜드 버튼들 */}
+      <div>
+        {brands.map((brand) => (
+          <button key={brand} onClick={() => setSelectedBrand(brand)}>
+            {brand}
+          </button>
+        ))}
+      </div>
+      {/* 모델 버튼들 */}
+      <div>
+        {model.map((model) => (
+          <button key={model} onClick={() => setSelectedModel(model)}>
+            {model}
+          </button>
+        ))}
+      </div>
+      {/* 선택된 상태 데이터 */}
+      <div>
+        <div>selectedType : {selectedType}</div>
+        <div>selectedBrand : {selectedBrand}</div>
+        <div>selectedModel : {selectedModel}</div>
+      </div>
     </>
   );
 }
