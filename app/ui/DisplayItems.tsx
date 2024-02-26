1e3e30f27a378ed73dcc6d868b9bcc12b47fb2c8
csm <csm009177@gmail.com>
2024-02-26 10:27:42 +0900

    디스플레이 컴포넌트 fetch 만들기

--- app/ui/DisplayItems.tsx
+++ app/ui/DisplayItems.tsx
@@ -3,15 +3,12 @@
 import React, { useContext, useEffect, useState } from "react";
 
 export default function DisplayItems() {
-  const [types, setTypes] = useState([]);
-  const [selectedType, setSelectedType] = useState([]);
-
-  const [brands, setBrands] = useState([]);
-  const [selectedBrands, setSelectedBrands] = useState([]);
-
   const [result, setResult] = useState("");
 
-  // 처음 랜더링될때 아이템 유형을 가져오는 비동기 함수입니다.
+  // 타입의 상태정보
+  const [types, setTypes] = useState([]);
+  const [selectedType, setSelectedType] = useState("");
+  // 처음 렌더링시 타입을 가져오는 비동기함수
   const fetchTypes = async () => {
     try {
       const response = await fetch("/fetchTypes");
@@ -21,8 +18,16 @@ export default function DisplayItems() {
       console.error("Error fetching types:", error);
     }
   };
+  // 컴포넌트가 처음 마운트시 타입을 가져오는 함수호출
+  useEffect(() => {
+    fetchTypes();
+    console.log(selectedType);
+  }, [selectedType]);
 
-  // 선택된 아이템 유형에 따라 브랜드를 가져오는 비동기 함수입니다.
+  // 브랜드의 상태정보
+  const [brands, setBrands] = useState([]);
+  const [selectedBrand, setSelectedBrand] = useState("");
+  // 선택된 타입의 브랜드들을 가져오는 함수
   const fetchBrands = async (selectedType) => {
     try {
       const response = await fetch(`/fetchBrands?type=${selectedType}`);
@@ -32,19 +37,73 @@ export default function DisplayItems() {
       console.error("Error fetching brands:", error);
     }
   };
-  // 컴포넌트가 처음 마운트될 때 아이템 유형을 가져오는 함수를 호출합니다.
+  // 컴포넌트가 처음 마운트시 브랜드를 가져오는 함수를 호출
   useEffect(() => {
-    fetchTypes();
-    console.log(selectedType)
+    fetchBrands(selectedType);
+    console.log(selectedType);
   }, [selectedType]);
+
+  // model의 상태정보
+  const [models, setModels] = useState([]);
+  const [selectedModel, setSelectedModel] = useState("");
+  // 선택된 브랜드의 모델들을 가져오는 비동기 함수입니다.
+  const fetchModels = async (selectedType, selectedBrand) => {
+    try {
+      const response = await fetch(
+        `/fetchModels?type=${selectedType}&brand=${selectedBrand}`
+      );
+      const data = await response.json();
+      setModels(data.models);
+    } catch (error) {
+      console.error("Error fetching models:", error);
+    }
+  };
+  // 컴포넌트가 처음 마운트시 model을 가져오는 함수를 호출
+  useEffect(() => {
+    fetchModels(selectedType, selectedBrand);
+    console.log(selectedType, selectedBrand);
+  }, [selectedType, selectedBrand]);
+
+  // 선택된 
+  const fetchModelInfos = async (    selectedType,    selectedBrand,    selectedModel  ) => {
+    try {
+      const response = await fetch( `/fetchModelInfos?type=${selectedType}&brand=${selectedBrand}&model=${selectedModel}`
+      );
+      const data = await response.json();
+    } catch (error) {
+      console.error("Error fetching modelInfo :", error);
+    }
+  };
+
+  // 컴포넌트가 처음 마운트시 model을 가져오는 함수를 호출
+  useEffect(() => {
+    fetchModelInfos(selectedType, selectedBrand,selectedModel);
+    console.log(selectedType, selectedBrand,selectedModel);
+  }, [selectedType, selectedBrand,selectedModel]);
 
   return (
     <>
-      {types.map((type) => (
-        <button key={type} onClick={() => setSelectedType(type)}>
-          {type}
-        </button>
-      ))}
+      <div>
+        {types.map((type) => (
+          <button key={type} onClick={() => setSelectedType(type)}>
+            {type}
+          </button>
+        ))}
+      </div>
+      <div>
+        {brands.map((brand) => (
+          <button key={brand} onClick={() => setSelectedBrand(brand)}>
+            {brand}
+          </button>
+        ))}
+      </div>
+      <div style={{display:"flex", flexDirection:"row", overflow:"scroll", overflowY:"hidden", maxWidth:"80vh"}}>
+        {models.map((model) => (
+          <button key={model} onClick={() => setSelectedModel(model)}>
+            {model}
+          </button>
+        ))}
+      </div>
     </>
   );
 }
--- serv.js
+++ serv.js
@@ -125,6 +125,7 @@ app.prepare().then(() => {
     });
   });
   
+  // item에 있는 type들을 가져오는 엔드포인트
   server.get("/fetchTypes", (req, res) => {
     const query = `SELECT DISTINCT type FROM item;`; // 중복되지 않는 type 값들을 가져오는 쿼리
     connection.query(query, (err, results, fields) => {
@@ -138,6 +139,7 @@ app.prepare().then(() => {
     });
   });
   
+  // item에 있는 선택된 타입에서 브랜드를 가져오는 엔드포인트
   server.get("/fetchBrands", (req, res) => {
     const { type } = req.query;
     const query = `SELECT DISTINCT brand FROM item WHERE type = ?;`;
@@ -152,13 +154,29 @@ app.prepare().then(() => {
     });
   });
 
+  // item에 있는 선택된 브랜드에서 모델을 가져오는 엔드포인트
   server.get("/fetchModels", (req, res) => {
     const { type, brand } = req.query;
-    const query = `SELECT DISTINCT model FROM item WHERE type = ? AND brand = ?;`;
+    const query = `SELECT * FROM item WHERE type = ? AND brand = ?;`;
     connection.query(query, [type, brand], (err, results, fields) => {
       if (err) {
         console.error("fetchModels error:", err);
         res.status(500).json({ message: "모델을 가져오는 중 문제가 발생했습니다." });
+        return;
+      }
+      const models = results.map(result => result.model);
+      res.status(200).json({ models });
+    });
+  });
+
+  // item에 있는 선택된 모델의 정보를 가져오는 엔드포인트
+  server.get("/fetchModelInfos", (req, res) => {
+    const { type, brand, model } = req.query;
+    const query = `SELECT * FROM item WHERE type = ? AND brand = ? AND model = ?;`;
+    connection.query(query, [type, brand, model], (err, results, fields) => {
+      if (err) {
+        console.error("fetchModelInfos error:", err);
+        res.status(500).json({ message: "모델 정보를 가져오는 중 문제가 발생했습니다." });
         return;
       }
       const models = results.map(result => result.model);
