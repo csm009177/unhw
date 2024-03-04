7ccc44c61d6b7731fb962d1d6fbe970dbc865826
csm <csm009177@gmail.com>
2024-03-04 12:39:49 +0900

    make layout structure

--- app/(pages)/layout.tsx
+++ app/(pages)/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import "./globals.css";
+import "../globals.css";
 
 export const metadata: Metadata = {
   title: "Create Desktop Project",
@@ -10,10 +10,8 @@ interface ChildrenProps {
   children: React.ReactNode;
 }
 
-export default function RootLayout({ children }: ChildrenProps) {
-  return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
-  );
+export default function MainLayout({ children }: ChildrenProps) {
+  return (<>
+  {children}
+  </>);
 }
