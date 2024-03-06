b2fd4eedd8c5fae40ebb5c51730e1be271a360be
csm <csm009177@gmail.com>
2024-03-06 19:46:24 +0900

    SearchBox comp - change type of types state

--- app/context/MainContext.tsx
+++ app/context/MainContext.tsx
@@ -1,6 +1,6 @@
 import { createContext, Dispatch, SetStateAction } from "react";
 
-// openContext
+// openContext - 토글이 열렸는지 불리언을 업데이트하는 함수
 export const openContext = createContext<{
   isOpen: boolean;
   setIsOpen: Dispatch<SetStateAction<boolean>>;
@@ -13,3 +13,8 @@ export const selectedProjectContext = createContext<{
 }>({ selectedPjtIndex: null, setSelectedPjtIndex: () => {} });
 // () => {}는 빈 함수를 나타냅니다. 이는 초기값으로 빈 함수를 설정해두었으며, 
 // 실제로는 컴포넌트에서 이 함수를 호출하여 상태를 업데이트할 때 해당 함수의 내용이 정의됩니다.
+
+export const typesContext = createContext<{
+  types: number | null;
+  setTypes: Dispatch<SetStateAction<number | null>>;
+}>({ types: null, setTypes: () => {} });
--- app/ui/Searchbox.tsx
+++ app/ui/Searchbox.tsx
@@ -6,11 +6,11 @@ interface SearchboxProps {
   // Props에 대한 설명 추가
 }
 
-// useState의 제네릭 타입을 설정하여 타입 안정성을 확보합니다.
+// useState의 제네릭 타입을 설정하여 타입 안정성 제공
 const Searchbox: React.FC<SearchboxProps> = () => {
-  // 타입을 문자열 배열로 설정합니다.
+  // types을 문자열의 배열로 설정합니다.
   const [types, setTypes] = useState<string[]>([]);
-  // 선택된 타입의 초기값을 빈 문자열로 설정합니다.
+  // selectedType의 타입을 넘버또는 빈것을 허용 초기값을 빈 문자열로 설정합니다.
   const [selectedType, setSelectedType] = useState<number | null>(null);
 
   const fetchTypes = async () => {
@@ -38,10 +38,7 @@ const Searchbox: React.FC<SearchboxProps> = () => {
       >
         types :
         {types.map((type, index) => (
-          <button
-            key={index}
-            onClick={() => setSelectedType(index)}
-          >
+          <button key={index} onClick={() => setSelectedType(index)}>
             {type}
           </button>
         ))}
