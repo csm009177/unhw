7b6da893280987029563977e71cba40d9675980a
csm <csm009177@gmail.com>
2024-03-07 11:35:47 +0900

    MainContext - write 모델의 상태값을 관리하는 ㅌ스트

--- app/context/MainContext.tsx
+++ app/context/MainContext.tsx
@@ -14,27 +14,39 @@ export const selectedProjectContext = createContext<{
 // () => {}는 빈 함수를 나타냅니다. 이는 초기값으로 빈 함수를 설정해두었으며, 
 // 실제로는 컴포넌트에서 이 함수를 호출하여 상태를 업데이트할 때 해당 함수의 내용이 정의됩니다.
 
-// typesContext - 타입의 넘버를 업데이트하는 함수
+// typesContext - 타입의 문자열을 업데이트하는 함수
 export const typesContext = createContext<{
   types: string[];
   setTypes: Dispatch<SetStateAction<string[]>>;
 }>({ types: [], setTypes: () => {} });
 
-// selectedTypeContext - 선택된 타입의 넘버를 업데이트하는 함수
+// selectedTypeContext - 선택된 타입의 문자열을 업데이트하는 함수
 export const selectedTypesContext = createContext<{
   selectedTypes: string| null;
   setSelectedTypes: Dispatch<SetStateAction< string | null>>;
 }>({ selectedTypes: null, setSelectedTypes: () => {} });
 
-// brandContext - 브랜드의 넘버를 업데이트하는 함수
+// brandContext - 브랜드의 문자열을 업데이트하는 함수
 export const brandsContext = createContext<{
   brands: string[];
   setBrands: Dispatch<SetStateAction<string[]>>;
 }>({ brands: [], setBrands: () => {} });
 
-// selectedBrandContext - 선택된 브랜드의 넘버를 업데이트하는 함수
+// selectedBrandContext - 선택된 브랜드의 문자열을 업데이트하는 함수
 export const selectedBrandsContext = createContext<{
   selectedBrands : string| null;
   setSelectedBrands: Dispatch<SetStateAction< string | null>>;
 }>({ selectedBrands: null, setSelectedBrands: () => {} });
+
+// modelsContext - 모델의 문자열을 업데이트하는 함수
+export const modelsContext = createContext<{
+  models: string[];
+  setBrands: Dispatch<SetStateAction<string[]>>;
+}>({ models: [], setBrands: () => {} });
+
+// selectedModelsContext - 선택된 모델의 문자열을 업데이트하는 함수
+export const selectedModelsContext = createContext<{
+  selectedModels : string| null;
+  setSelectedModels: Dispatch<SetStateAction< string | null>>;
+}>({ selectedModels: null, setSelectedModels: () => {} });
 
