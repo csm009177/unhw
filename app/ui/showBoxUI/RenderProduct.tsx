bb93b208c1fcaaf715e2bfb5f2328226bb99f5bd
csm <csm009177@gmail.com>
2024-03-09 15:58:14 +0900

    RenderProduct - 1.apply prevState for devide each product 2.make handleDelete function for delete each product

--- app/ui/showBoxUI/RenderProduct.tsx
+++ app/ui/showBoxUI/RenderProduct.tsx
@@ -1,34 +1,38 @@
 "use client";
 
-import { selectedProductContext } from "@/app/context/MainContext";
-import React, { useContext, useEffect } from "react";
+//! RenderProduct selectedProduct의 상태를 가져와서 화면에 표시하는 컴포넌트입니다 
 
-/**
- *! RenderProduct selectedProduct의 상태를 가져와서 화면에 표시하는 컴포넌트입니다
- */
+import React, { useContext } from "react";
+import { selectedProductContext } from "@/app/context/MainContext";
 
 const RenderProduct: React.FC = () => {
   const { selectedProduct, setSelectedProduct } = useContext(
     selectedProductContext
   );
-  useEffect(() => {
-    console.log(selectedProduct);
-  }, [selectedProduct, setSelectedProduct]);
 
-  const handleDelete = () => {
-  }
+  const handleDelete = (index: number) => {
+    setSelectedProduct((prevState) => {
+      // 새로운 배열을 생성하고, 삭제할 인덱스의 요소를 제외한 나머지 요소들을 복사합니다.
+      const newState = prevState.filter((_, idx) => idx !== index);
+      return newState;
+    });
+  };
+
   return (
     <>
-      {/* 타입 버튼들을 화면에 표시합니다. */}
       <div
         style={{
           display: "flex",
           flexDirection: "row",
           justifyContent: "space-between",
-        }}>
-        {selectedProduct ? (
-          <button onClick={() => {}}>{selectedProduct}</button>
-        ) : null}
+        }}
+      >
+        {/* 각각의 선택된 모델을 버튼으로 표시합니다. */}
+        {selectedProduct.map((product, index) => (
+          <button key={index} onClick={() => handleDelete(index)}>
+            {product}
+          </button>
+        ))}
       </div>
     </>
   );
