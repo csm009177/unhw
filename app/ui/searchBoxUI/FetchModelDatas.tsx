6dbbb2ab535d941a7836ecfad300780c81ff98ee
csm <csm009177@gmail.com>
2024-03-11 16:14:30 +0900

    FetchModelDatas - move Detail title upper

--- app/ui/searchBoxUI/FetchModelDatas.tsx
+++ app/ui/searchBoxUI/FetchModelDatas.tsx
@@ -21,16 +21,28 @@ interface ModelData {
 }
 
 const FetchModelDatas: React.FC = () => {
-  const {modelDatas, setModelDatas} = useContext(modelDatasContext)
+  const { modelDatas, setModelDatas } = useContext(modelDatasContext);
 
   const { selectedTypes, setSelectedTypes } = useContext(selectedTypesContext);
-  const { selectedBrands, setSelectedBrands } = useContext(selectedBrandsContext);
-  const { selectedModels, setSelectedModels } = useContext(selectedModelsContext);
-  const {selectedProduct, setSelectedProduct} = useContext(selectedProductContext);
-  
-  const fetchModelDatas = async (selectedTypes: string, selectedBrands:string, selectedModels:string) => {
+  const { selectedBrands, setSelectedBrands } = useContext(
+    selectedBrandsContext
+  );
+  const { selectedModels, setSelectedModels } = useContext(
+    selectedModelsContext
+  );
+  const { selectedProduct, setSelectedProduct } = useContext(
+    selectedProductContext
+  );
+
+  const fetchModelDatas = async (
+    selectedTypes: string,
+    selectedBrands: string,
+    selectedModels: string
+  ) => {
     try {
-      const response = await fetch(`/fetchModelDatas?type=${selectedTypes}&brand=${selectedBrands}&model=${selectedModels}`);
+      const response = await fetch(
+        `/fetchModelDatas?type=${selectedTypes}&brand=${selectedBrands}&model=${selectedModels}`
+      );
       const data = await response.json();
       setModelDatas(data.modelDatas); // models 배열 설정
     } catch (error) {
@@ -41,49 +53,53 @@ const FetchModelDatas: React.FC = () => {
   useEffect(() => {
     if (selectedTypes && selectedBrands && selectedModels) {
       fetchModelDatas(selectedTypes, selectedBrands, selectedModels);
-      if(selectedModels){
-        console.log("selectedModels : "+ selectedModels)
+      if (selectedModels) {
+        console.log("selectedModels : " + selectedModels);
       }
     }
   }, [selectedTypes, selectedBrands, selectedModels]);
 
-  const handleClickProduct = async (selectedModels:[]) =>{
-    setSelectedProduct((prevState) => [...prevState, selectedModels])
-    console.log(selectedModels)
-  }
+  const handleClickProduct = async (selectedModels: []) => {
+    setSelectedProduct((prevState) => [...prevState, selectedModels]);
+    console.log(selectedModels);
+  };
 
   return (
-    <div
-    style={{
-      display: "flex",
-      flexDirection: "row",
-      justifyContent: "space-between",
-      overflowX:'hidden',
-      maxWidth:"100vh",
-      overflow:'scroll'
-    }}
-  >
-    <div>Detail : </div>
-    {modelDatas ? (
-      modelDatas.map((info:ModelData, index:number) => (
-        <button 
-        key={index} onClick={() => handleClickProduct(selectedModels)}>
-          <ul>
-            <li>Type: {info.type}</li>
-            <li>Part Number: {info.part_number}</li>
-            <li>Brand: {info.brand}</li>
-            <li>Model: {info.model}</li>
-            <li>Rank: {info.rank}</li>
-            <li>Benchmark: {info.benchmark}</li>
-            <li>Samples: {info.samples}</li>
-          </ul>
-          <a href={info.url}>Link Button</a>
-        </button>
-      ))
-    ) : (
-      <p>No model information available.</p>
-    )}
-  </div>
+    <>
+      <div>Detail : </div>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "space-between",
+          overflowX: "hidden",
+          maxWidth: "100vh",
+          overflow: "scroll",
+        }}
+      >
+        {modelDatas ? (
+          modelDatas.map((info: ModelData, index: number) => (
+            <button
+              key={index}
+              onClick={() => handleClickProduct(selectedModels)}
+            >
+              <ul>
+                <li>Type: {info.type}</li>
+                <li>Part Number: {info.part_number}</li>
+                <li>Brand: {info.brand}</li>
+                <li>Model: {info.model}</li>
+                <li>Rank: {info.rank}</li>
+                <li>Benchmark: {info.benchmark}</li>
+                <li>Samples: {info.samples}</li>
+              </ul>
+              <a href={info.url}>Link Button</a>
+            </button>
+          ))
+        ) : (
+          <p>No model information available.</p>
+        )}
+      </div>
+    </>
   );
 };
 
